91f65df60d06de0ef7c7b7c5319ed8f9d20d7850
유경선 <106594608+YuGyeongseon@users.noreply.github.com>
2023-07-06 22:20:26 +0900

    병합본
    병합본 수정본

---
+++ UICombineVer1.0.0/Assets/Script/repair_button.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class repair_button : MonoBehaviour
+{
+
+    public void repaired()
+    {
+        Settings.complete_doll += repair_station.repair_doll;
+        repair_station.repair_doll = 0;
+    }
+    public void sell_now()
+    {
+        Settings.coin += Settings.incomplete_doll;
+        Settings.incomplete_doll = 0;
+    }
+
+    public void advertise()
+    {
+        Debug.Log("ad");
+        Settings.coin += 3 * Settings.incomplete_doll;
+        Settings.incomplete_doll = 0;
+    }
+    public void Upgrade()
+    {
+        if(repair_station.station_level == 1&&Settings.coin>=1000) {
+            repair_station.station_level++;
+            Settings.coin -= 1000;
+        }
+        if (repair_station.station_level == 2 && Settings.coin >= 1500)
+        {
+            repair_station.station_level++;
+            Settings.coin -= 1500;
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
